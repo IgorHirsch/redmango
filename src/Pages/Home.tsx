ae9144462abeeb6479aa8d479ffdf0dc0fa4a560
IgorHirsch <g1rsh.egor@gmail.com>
2025-08-29 13:39:17 +0200

    HomePage

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { MenuItemList } from "../Components/Page/MenuItems";
+
+function Home() {
+  return (
+    <div>
+      <div className="container">
+        <MenuItemList />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
